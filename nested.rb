7968b6b66d7b8cab54dac5dcb60fe3b5cf94d33c
Shamel Lakin <christlife4u@gmail.com>
2019-09-22 19:13:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,8 +66,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
+programmer_hash[:yukihiro_matsumoto] = [:known_for] = "ruby", [:languages] ="LISP", "C"
 programmer_hash = 
-	programmer_hash[:yukihiro_matsumoto] = 
+	
  		{
         :grace_hopper => {
           :known_for => "COBOL",
